c44c3cad8dcc1c4a5f32fc80b138131149e7bd16
Rizwan Shah <rix.jafri@gmail.com>
2025-01-28 15:27:08 +0500

    initial code

---
+++ Memo Cloud/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memo Cloud
+//
+//  Created by Rizwan Shah on 28/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
